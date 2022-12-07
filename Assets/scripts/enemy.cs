519a78933cad8c70e0db559a2531b2241aea71ca
JesseOpSchool <91886106+JesseOpSchool@users.noreply.github.com>
2022-12-07 11:36:32 +0100

    Animation enemy add

--- Assets/scripts/enemy.cs
+++ Assets/scripts/enemy.cs
@@ -6,11 +6,9 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public int hp = 2;
-    public float speed = 5f;
+    public float speed = -5f;
     public float strarting;
 
-    float diry = 1f;
-    bool isAlive = true;
 
     Animator anim;
 
@@ -18,49 +16,21 @@ public class Enemy : MonoBehaviour
     void Start()
     {
         anim = GetComponentInChildren<Animator>();
+        GoToPosition();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isAlive)
-        {
-
-
-            transform.Translate(transform.up * diry * speed * Time.deltaTime);
-
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up * diry, 0.6f);
-
-            Debug.DrawRay(transform.position, transform.up * 0.6f * diry, Color.blue);
-
-
-            if (hit.collider != null)
-            {
-                if (hit.collider.CompareTag("wall"))
-                {
-                    diry *= -1f;
-                }
-            }
-
-        }
+        
 
     }
 
-    public void takeDamage()
+    public void GoToPosition()
     {
-        hp--;
-        if (hp <= 0)
-        {
-            die();
-        }
+       
     }
 
-    void die()
-    {
-        isAlive = false;
-        anim.SetBool("IsAlive", isAlive);
-        GetComponent<BoxCollider2D>().enabled = false;
-        GetComponent<Rigidbody2D>().gravityScale = 0;
-
-    }
+  
 }
